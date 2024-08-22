60ecc2caef4c631e52672770e510f3f8a7a06d61
IAmLaxel <thisislaxel@gmail.com>
2024-08-22 15:17:16 +0800

    fixed spelling mistake

--- scripts/HIdeUnhideContent.js
+++ scripts/HIdeUnhideContent.js
@@ -1,6 +1,6 @@
 function toggleVisibility(elementId, buttonId) {
-    const element = document.getElementById(elementId);
-    const elementButton = document.getElementById(buttonId);
-    element.classList.toggle('hidden');
-    elementButton.classList.toggle('open')
+  const element = document.getElementById(elementId);
+  const elementButton = document.getElementById(buttonId);
+  element.classList.toggle("hidden");
+  elementButton.classList.toggle("open");
 }
