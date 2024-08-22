b67bbccae1d35c8e2d0520d9982b6041c430aa11
IAmLaxel <thisislaxel@gmail.com>
2024-08-22 15:06:16 +0800

    added collapsable content

---
+++ scripts/HIdeUnhideContent.js
@@ -0,0 +1,6 @@
+function toggleVisibility(elementId, buttonId) {
+    const element = document.getElementById(elementId);
+    const elementButton = document.getElementById(buttonId);
+    element.classList.toggle('hidden');
+    elementButton.classList.toggle('open')
+}
